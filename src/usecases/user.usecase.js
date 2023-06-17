a24a02df932068dba99b1582cbc4a387b8f5cd2d
Alejandra Paez <alepaezdev@gmail.com>
2023-06-17 12:08:49 -0600

    feat: added hashed password to user usecase

--- src/usecases/user.usecase.js
+++ src/usecases/user.usecase.js
@@ -2,10 +2,12 @@
  * 1- Se importa el modelo
  * 2- Se hacen las acciones(logica)
  */
-
+const bcrypt = require("bcrypt");
 const User = require("../models/user.model");
 
-const create = (data) => {
+const create = async (data) => {
+  const hashedPassword = await bcrypt.hash(data.password, 10);
+  data.password = hashedPassword;
   const user = User.create(data);
   return user;
 }
